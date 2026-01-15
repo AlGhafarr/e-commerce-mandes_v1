fd1e53a5f45956502ca7890edcfdbd00ddb27dc6
AlGhafarr <mhmmdalghfr15@gmail.com>
2026-01-15 18:18:14 +0700

    third commit

---
+++ snack-ecommerce/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          orange: '#F87B1B',
+          cream: '#FCF9EA',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
